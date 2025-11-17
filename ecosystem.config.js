98b148cd754c6ac0ea0dad08333634336da8987c
Jo Vinkenroye <jestersimpps@gmail.com>
2025-11-17 17:09:23 +0100

    pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  apps: [{
+    name: 'copyscalper',
+    script: './dist/monitor.js',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'production'
+    },
+    error_file: './logs/pm2-error.log',
+    out_file: './logs/pm2-out.log',
+    log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
+    merge_logs: true,
+    min_uptime: '10s',
+    max_restarts: 10,
+    restart_delay: 4000
+  }]
+};
